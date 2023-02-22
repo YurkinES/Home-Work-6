709c55a769317a5b94880e022933ce055d2aa393
YurkinES <6eJlomop@inbox.ru>
2023-02-22 12:07:17 +0300

    Stage 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task #1
+    for (int a = 1; a <= 10; a++) {
+        System.out.println(a);
+    }
+    //Task #2
+    for (int b = 10; b > 0; b-- ) {
+        System.out.println(b);
+    }
+    //Task #3
+    for (int c = 0; c < 17 ; c = c + 2 ) {
+        System.out.println(c);
+    }
+    //Task #4
+    for (int d = 10; d > -10; d-- ){
+        System.out.println(d);
+    }
+    //Task #5
+    for (int y = 1904; y < 2096; y = y + 4){
+        System.out.println(y + " является високосным годом");
+    }
+    //Task #6
+    for (int n = 7; n < 100; n = n + 7) {
+        System.out.println(n);
+    }
+    //Task #7
+    for (int n = 1; n < 1000; n = n * 2){
+        System.out.println(n);
+    }
+    }
+}
